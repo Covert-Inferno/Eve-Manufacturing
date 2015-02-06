11d8655650196779906c74d8d56d9f4e31cf9494
Jeffrey Nelson <asgardolympian@gmail.com>
2015-02-05 20:01:49 -0500

    Added Table Templates

---
+++ bin/tables/classifications.js
@@ -0,0 +1,14 @@
+{"NAME":"classifications",
+"PARAMS":
+[
+{"0": "id INT PRIMARY KEY AUTO_INCREMENT"},
+{"1":"classification VARCHAR(64) COLLATE utf8_unicode_ci"},
+{"ID": "id INT PRIMARY KEY AUTO_INCREMENT"},
+{"NAME":"classification VARCHAR(64) COLLATE utf8_unicode_ci"}
+],
+
+"ARGS":
+[
+{"0": "classification"}
+{"NAME": "classification"}
+]}
---
+++ bin/tables/items.js
@@ -0,0 +1,27 @@
+{"NAME":"classification_relations",
+"PARAMS":
+[
+{"ID": "id INT PRIMARY KEY AUTO_INCREMENT"},
+{"NAME":"c_name VARCHAR(64) COLLATE utf8_unicode_ci"},
+{"TIER1":"tier1 INT"},
+{"TIER2":"tier2 INT"},
+{"TIER3":"tier3 INT"},
+{"TIER4":"tier4 INT"},
+{"TIER5":"tier5 INT"}
+],
+
+"ARGS":
+[
+{"0":"c_name"},
+{"1":"tier1"},
+{"2":"tier2"},
+{"3":"tier3"},
+{"4":"tier4"},
+{"5":"tier5"},
+{"NAME":"c_name"},
+{"TIER1":"tier1"},
+{"TIER2":"tier2"},
+{"TIER3":"tier3"},
+{"TIER4":"tier4"},
+{"TIER5":"tier5"}
+]}
